0f383e239cd01b7ac6d0c799afd98d46f37dc60a
Winnie Song <w.jy.song@gmail.com>
2014-06-17 09:05:50 -0400

    Collision test and hallways model in CameraTest.unity

---
+++ DrunkWalk/Assets/Collision.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+	public GameObject ouch;
+	public Animator ouchAnim;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log("Collision");
+		ouchAnim.SetTrigger("Ouch");
+		Yell();
+		if (col.tag == "Wall") {
+			Debug.Log("Wall Collision");
+		}
+
+	}
+
+	void OnTriggerExit(Collider col) {
+		Debug.Log("ExitCollision");
+		if (col.tag == "Wall") {
+			Debug.Log("Exit Wall Collision");
+		}
+	}
+
+	public void Yell () {
+		Debug.Log("Instantiate Yell");
+		GameObject newOuch = Instantiate(ouch, transform.position + (0.8f * Vector3.up) + (2.5f * Vector3.forward) + (0.5f * Vector3.right), Quaternion.identity) as GameObject;
+		this.StartCoroutine(this.DestroyYell(newOuch, 1.0f));
+	}
+
+	IEnumerator DestroyYell(GameObject _yell, float delay) {
+		yield return new WaitForSeconds(delay);
+		Destroy(_yell);
+	}
+}
