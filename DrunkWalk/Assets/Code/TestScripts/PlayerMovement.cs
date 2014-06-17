d14da45de7d62b409dd8e882ebb02af34ebb3bd4
acbui <acqbui@gmail.com>
2014-06-17 15:15:34 -0400

    Modified Scripts

--- DrunkWalk/Assets/Code/TestScripts/PlayerMovement.cs
+++ DrunkWalk/Assets/Code/TestScripts/PlayerMovement.cs
@@ -7,8 +7,8 @@ public class PlayerMovement : MonoBehaviour {
 	
 	public Vector3 mouse;		// current mouse position on screen
 	public float delay; 		// time delay between feet movement and head movement 
-	public float hinc = 0.5f;			// force increment for head
-	public float finc = 0.5f; 			// force increment for feet
+	public float hinc = 0.5f;	// force increment for head
+	public float finc = 0.5f; 	// force increment for feet
 
 	public Rigidbody rhead;		// rigidbody at the head of the player
 	public Rigidbody rfeet;		// rigidbody at the feet of the player
@@ -18,22 +18,13 @@ public class PlayerMovement : MonoBehaviour {
 	private int halfWidth; 		// half the width of screen
 	private int halfHeight; 	// half the height of screen
 
-	private enum Dir { up, right, left, back }; 
-	private int direction; 
+	private enum Dir { forward, right, left, back }; 
+	public int direction; 
 	private bool fallen;
-	private bool walkfwd;
-	private bool walkr;
-	private bool walkl;
-	private bool walkbk;
 
 	void Start () {
 		halfWidth = Screen.width / 2; 
 		halfHeight = Screen.height / 2; 
-
-		//rhead = GetComponent<Rigidbody> ();
-		//rfeet = GetComponent<Rigidbody> ();
-		//fhead = GetComponentInChildren<ConstantForce> ();
-		//ffeet = GetComponentInChildren<ConstantForce> ();
 
 		fallen = false;
 	}
@@ -45,85 +36,76 @@ public class PlayerMovement : MonoBehaviour {
 		if (isLeaningTooMuch()) {
 			// FALL = rotate camera down
 		}
-		//else {
-		print ("0. got mouse position ");
-			direction = getLeanDirection (mouse); 
-		print ("1. got direction");
-			moveHead (direction);
-		print ("2. moved head"); 
-			StartCoroutine(delayFeet ()); 
-		print ("3. delayed feet");
-			moveFeet (direction); 
-		print ("4. moved feet"); 
+		//else { //print ("0. got mouse position ");
+			direction = getLeanDirection (); 	//print ("1. got direction");
+			moveHead (direction); 					//print ("2. moved head"); 
+			StartCoroutine(delayFeet ()); 			//print ("3. delayed feet");
+			moveFeet (direction); 					//print ("4. moved feet"); 
 		//}
 	}
 
-	private int getLeanDirection(Vector3 mouse){
-		print("entered get direction");
+	private int getLeanDirection(){	//Vector3 mouse){	//print("entered get direction");
 		
 		if (mouse.y < halfHeight) {	// if mouse in lower half of screen, leaning back 
-			print ("leaning back");
-			return (int) Dir.back; 
-		}
-		else {	// if mouse is in top half, check right/left lean as well
-			if (Mathf.Abs(mouse.x - halfWidth) < Mathf.Abs(mouse.y - halfHeight)){	// if the mouse is higher up than it is to the side
-				print ("leaning forward");
-				return (int) Dir.up; 
+
+			if (Mathf.Abs(mouse.x - halfWidth) < Mathf.Abs(mouse.y - halfHeight)){ 		// print ("leaning forward");
+				return (int) Dir.back; 
 			}
 			else {
-				if (mouse.x >= halfWidth){
-					print ("leaning right");
+				if (mouse.x >= halfWidth){		// print ("leaning right");
 					return (int) Dir.right; 
 				}
-				else {
-					print ("leaning left");
+				else {							// print ("leaning left");
 					return (int) Dir.left; 
 				}
 			}
-			print ("DEFAULT: leaning back");
-			return (int) Dir.back; 
+		}
+		else {	// if mouse is in top half, check right/left lean as well
+			if (Mathf.Abs(mouse.x - halfWidth) < Mathf.Abs(mouse.y - halfHeight)){	// print ("leaning forward");
+				return (int) Dir.forward; 
+			}
+			else {
+				if (mouse.x >= halfWidth){ 	// print ("leaning right");
+					return (int) Dir.right; 
+				}
+				else {						// print ("leaning left");
+					return (int) Dir.left; 
+				}
+			}
 		}
 	}
 
 	// !! NB: FOR NOW IF LEAN BACK, STOP PLAYER
 
-	private bool isLeaningTooMuch(){
-		print ("checking lean");
+	private bool isLeaningTooMuch(){ //print ("checking lean");
 		Vector3 vertVec = new Vector3 (rfeet.position.x, rhead.position.y, rfeet.position.z); 
 		float angle = Vector3.Angle (vertVec, rhead.position); 
-		if (angle >= 30.0f) {
-			print ("FALLEN!");
+		if (angle >= 30.0f) { 	// print ("FALLEN!");
 			fallen = true; 
 			return true;
-		}
-		print ("STILL STANDING");
+		} 						// print ("STILL STANDING");
 		return false; 
 	}
 
 
 	// depending on the direction of the lean, set a constantforce on the rigidbody of the head 
-	private void moveHead (int direction){
-		print ("moving head ");
+	private void moveHead (int direction){	//print ("moving head ");
 
 		switch (direction) {
 			
-		case (int) Dir.up:
-			print ("moving head forward");
+		case (int) Dir.forward:				//print ("moving head forward");
 			rhead.AddForce (0, 0, hinc);  
 			break;
 			
-		case (int) Dir.right:
-			print ("moving head to the right");
+		case (int) Dir.right:				//print ("moving head to the right");
 			rhead.AddForce (hinc, 0, 0); 
 			break;
 			
-		case (int) Dir.left:
-			print ("moving head to the left");
+		case (int) Dir.left:				//print ("moving head to the left");
 			rhead.AddForce (-hinc, 0, 0); 
 			break;
 			
-		case (int) Dir.back:
-			print ("stopping head movement");
+		case (int) Dir.back:				//print ("stopping head movement");
 			rhead.AddForce (0, 0, 0); 
 			break; 
 			
@@ -134,29 +116,25 @@ public class PlayerMovement : MonoBehaviour {
 
 
 	// depending on the direction of the lean, set a constantforce on the rigidbody of the feet 
-	private void moveFeet (int direction){
-		print ("moving feet");
+	private void moveFeet (int direction){			//print ("moving feet");
 		switch (direction) {
 			
-		case (int) Dir.up:
-			print ("moving feet forward");
+		case (int) Dir.forward:						//print ("moving feet forward");
 			rfeet.AddForce (0, 0, finc); 
 			break;
 			
-		case (int) Dir.right:
-			print ("moving feet right");
+		case (int) Dir.right:						//print ("moving feet right");
 			rfeet.AddForce (finc, 0, 0); 
 			break;
 			
-		case (int) Dir.left:
-			print ("moving feet left");
+		case (int) Dir.left:						//print ("moving feet left");
 			rfeet.AddForce (-finc, 0, 0);  
 			break;
 
 		// if player leans back, the feet will match the feet 
-		case (int) Dir.back:
-			print ("stopping feet under head");
+		case (int) Dir.back:						//print ("stopping feet under head");
 			ffeet.force.Set (0, 0, 0); 
+
 			rfeet.position = new Vector3 (rhead.position.x, rfeet.position.y, rhead.position.z); 
 			break; 
 			
@@ -166,8 +144,7 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	// delay the movement of the feet after the movement of the head 
-	private IEnumerator delayFeet (){
-		print ("delaying");
+	private IEnumerator delayFeet (){				//print ("delaying");
 		yield return new WaitForSeconds(delay);
 		//yield break; 
 	}
