7c2421f2054a60cccd4b5e12feebf5ce606a2a5f
Winnie Song <w.jy.song@gmail.com>
2014-06-17 09:29:04 -0400

    Scoring system: wall: -100 pts

--- DrunkWalk/Assets/Collision.cs
+++ DrunkWalk/Assets/Collision.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class Collision : MonoBehaviour {
 
+	//Overall Score
+	public static int score;
+
+	//Hurt Sound
 	public GameObject ouch;
 	public Animator ouchAnim;
 
+	private static bool yelling = false;
+
 	// Use this for initialization
 	void Start () {
+		score = 10000;
 	
 	}
 	
@@ -16,31 +23,40 @@ public class Collision : MonoBehaviour {
 	
 	}
 
+	//When colliding with something:
 	void OnTriggerEnter(Collider col) {
 		Debug.Log("Collision");
 		ouchAnim.SetTrigger("Ouch");
-		Yell();
+		
+		//If collision is against a wall:
 		if (col.tag == "Wall") {
-			Debug.Log("Wall Collision");
+			score -= 100;
+			Debug.Log("Wall Collision - " + score);
+		}
+
+		//If not currently yelling:
+		if (yelling == false){
+			Yell();
+		} else if (yelling == true){
+			StopAllCoroutines();
 		}
 
 	}
-
+	//When not:
 	void OnTriggerExit(Collider col) {
-		Debug.Log("ExitCollision");
-		if (col.tag == "Wall") {
-			Debug.Log("Exit Wall Collision");
-		}
+		Debug.Log("No Longer Colliding");
 	}
 
+	//Instantiate a hurt sound
 	public void Yell () {
-		Debug.Log("Instantiate Yell");
 		GameObject newOuch = Instantiate(ouch, transform.position + (0.8f * Vector3.up) + (2.5f * Vector3.forward) + (0.5f * Vector3.right), Quaternion.identity) as GameObject;
 		this.StartCoroutine(this.DestroyYell(newOuch, 1.0f));
 	}
 
+	//Destroy the hurt sound
 	IEnumerator DestroyYell(GameObject _yell, float delay) {
 		yield return new WaitForSeconds(delay);
 		Destroy(_yell);
+		yelling = true;
 	}
 }
