dbd5caa8ddca522a6ec4e462f7b885b12d1c0695
acbui <acqbui@gmail.com>
2014-07-08 13:49:09 -0400

    Added MoveFeet script to prefab

---
+++ DrunkWalk/Assets/Scripts/moveFeet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveFeet : MonoBehaviour {
+
+	private Rigidbody rb; 
+
+	void Start () {
+		rb = GetComponent <Rigidbody> (); 
+	}
+
+	void Update () {
+		transform.position = rb.position; 
+	}
+}
