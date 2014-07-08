58504a1e7a7651859dd1195ceb7b66adbe35f192
acbui <acqbui@gmail.com>
2014-07-08 16:30:21 -0400

    Added a frame delay to Rotation script

--- DrunkWalk/Assets/Scripts/Rotation.cs
+++ DrunkWalk/Assets/Scripts/Rotation.cs
@@ -12,10 +12,15 @@ public class Rotation : MonoBehaviour {
 	private enum Turn { left, right }; 
 	public int direction; 
 
-
+	// delay before checking rotation after rotated
+	private bool rotating; 
+	public float rotateDelay; 
+	public float currentFrame; 
+	
 	// Use this for initialization
 	void Start () {
-	
+		rotating = false; 
+		currentFrame = 0; 
 	}
 	
 	// Update is called once per frame
@@ -24,6 +29,14 @@ public class Rotation : MonoBehaviour {
 		direction = getTurnDirection();
 		turnHead (direction);
 
+		if (rotating) {
+			currentFrame++; 
+
+			if (currentFrame >= rotateDelay){
+				rotating = false; 
+				currentFrame = 0; 
+			}
+		}
 		//Debug.Log(UniMove.gy);
 		
 	}
@@ -39,22 +52,25 @@ public class Rotation : MonoBehaviour {
 	}
 
 	private void turnHead (int direction){
-		switch (direction){
+		if (!rotating){
+			switch (direction){
 
-		case (int) Turn.left:
-			//Debug.Log("Turning Left?");
+			case (int) Turn.left:
+				//Debug.Log("Turning Left?");
 
-			transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y - camInc, transform.rotation.z, transform.rotation.w); 
-			break;
+				transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y - camInc, transform.rotation.z, transform.rotation.w); 
+				break;
 
-		case (int) Turn.right:
-			//Debug.Log("Turning Right?");
+			case (int) Turn.right:
+				//Debug.Log("Turning Right?");
 
-			transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y + camInc, transform.rotation.z, transform.rotation.w); 
-			break;
+				transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y + camInc, transform.rotation.z, transform.rotation.w); 
+				break;
 
-		default:
-			break;
+			default:
+				break;
+			}
+			rotating = true; 
 		}
 	}
 }
