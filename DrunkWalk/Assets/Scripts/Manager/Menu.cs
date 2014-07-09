e1254ca5150c61acffb3703bbe1288d264c24c45
wjys <w.jy.song@gmail.com>
2014-07-09 09:53:25 -0400

    Game Manager shit

---
+++ DrunkWalk/Assets/Scripts/Manager/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	private Tween.Interpolation flickInterpolation = Tween.InOut(Tween.Sinusoidal);
+
+	public delegate void Command ();
+
+	public struct Item {
+		public string name;
+		public Command command;
+		public Item (string name, Command command) {
+			this.name = name;
+			this.command = command;
+		}
+	}
+
+	public void GUIMenu (int idx, float pos, float interval, Item[] items, float time) {
+		int i = 0;
+		foreach (var item in items) {
+			if (i == idx) {
+				GUI.color = Utils.SetAlpha(GUI.color, flickInterpolation(time % 1f));
+			} else GUI.color = Utils.SetAlpha(GUI.color, 1);
+			GUI.Label(new Rect(0, pos + i * interval, Screen.width, Screen.height), item.name);
+			++i;
+		}
+	}
+}
