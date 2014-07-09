30dcd4ac363fea36306a409b07abc7efe5e12834
wjys <w.jy.song@gmail.com>
2014-07-09 12:06:23 -0400

    set playermove to ingame!!

--- DrunkWalk/Assets/Scripts/Manager/InGame.cs
+++ DrunkWalk/Assets/Scripts/Manager/InGame.cs
@@ -2,40 +2,26 @@
 using System.Collections.Generic;
 
 public class InGame : MonoBehaviour {
+
 	public bool paused {
 		set {
 			// pause animator component if possible
-			Animator animator = GetComponent<Animator>();
-			if (animator != null) {
-				if (value) {
-					animatorEnabledStatus = animator.enabled;
-					animator.enabled = false;
-				} else {
-					animator.enabled = animatorEnabledStatus;
-				}
-			}
+			Transform trans = GetComponent<Transform>();
+			TopplingForce tf = GetComponent<TopplingForce>();
+			trans = gameObject.transform;
+			tf.enabled = false; 
+			
 			// pause animation component if possible
-			Animation animation = GetComponentInChildren<Animation>();
-			if (animation != null) {
-				if (value) {
-					foreach (AnimationState st in animation) {
-						animationSpeedStatus[st] = st.speed;
-						st.speed = 0.0f;
-					}
-				} else {
-					foreach (AnimationState st in animation) {
-						st.speed = animationSpeedStatus[st];
-					}
-				}
-			}
+			Transform transChild = GetComponentInChildren<Transform>();
+			transChild = transform;
+
+			tf.enabled = false; 
 
 			_paused = value;
 		}
 		get { return _paused; }
 	}
 	private bool _paused = false;
-	private bool animatorEnabledStatus;
-	private Dictionary<AnimationState, float> animationSpeedStatus = new Dictionary<AnimationState, float>();
 	void Update () {
 		if (!paused) GameUpdate();
 	}
