ec7bcee4888f1ac754a5e4ff9fbb9e2b8a917f79
wjys <w.jy.song@gmail.com>
2014-07-15 12:27:22 -0400

    Script: Shatter Objects on Collision with Floor

---
+++ DrunkWalk/Assets/Scripts/BreakObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ONLY ATTACHED TO OBJECTS THAT WILL SHATTER (FRAMES, VASES, ETC.) 
+// shatter on collision with the floor 
+
+public class BreakObject : MonoBehaviour {
+
+	public Transform brokenVase; 
+	public float shatterVelocity; 
+	public float shatterRadius;
+	public float shatterForce; 
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.relativeVelocity.magnitude > shatterVelocity) { return; } // why is this syntax wrong?
+		else {
+			Destroy (gameObject); 
+			GameObject broken = Instantiate(brokenVase, transform.position, transform.rotation);
+			Rigidbody[] pieces = broken.GetComponentsInChildren<Rigidbody>(); 
+			Vector3 shatterPos = transform.position; 
+
+			foreach (Rigidbody piece in pieces){
+				piece.AddExplosionForce (shatterForce, shatterPos, shatterRadius);
+				piece.velocity = rigidbody.velocity;
+				piece.angularVelocity = rigidbody.angularVelocity; 
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider col){
+		if (col.tag == "Floor"){
+			GameObject broken = Instantiate(brokenVase, transform.position, transform.rotation);
+			Rigidbody[] pieces = broken.GetComponentsInChildren<Rigidbody>(); 
+			Vector3 shatterPos = transform.position; 
+			
+			foreach (Rigidbody piece in pieces){
+				piece.AddExplosionForce (shatterForce, shatterPos, shatterRadius);
+				piece.velocity = rigidbody.velocity;
+				piece.angularVelocity = rigidbody.angularVelocity; 
+			}
+		}
+	}
+}
