f66aa70e648eb2a3c7c0a574386404e68594a3ad
Anh Chi Bui <acqbui@gmail.com>
2014-07-17 12:55:21 -0400

    Wobble up/down mostly works

--- DrunkWalk/Assets/Scripts/HeadWobble.cs
+++ DrunkWalk/Assets/Scripts/HeadWobble.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class HeadWobble : MonoBehaviour {
 
-
-	public DrunkMovement player; 
+	public GameObject player; 
+	public DrunkMovement dm; 
 	public float targetInc; 
+	private Vector3 currentPosition; 
 
 	public bool stopWobble; 
 	private enum Dir { forward, right, left, back };
@@ -16,22 +17,42 @@ public class HeadWobble : MonoBehaviour {
 
 	void Update () {
 		if (!stopWobble)
-			headTilt (player.direction); 
+			headTilt (dm.direction); 
 	}
 
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * NO RETURN. ARG: int lean = the direction that the player is moving in
+	 * 
+	 * -------------------------------------------------------------------------------------------------------------------------- */
 	private void headTilt(int lean){
 		switch (lean) {
 		case (int) Dir.forward:
-			transform.position = new Vector3 (transform.position.x, transform.position.y - (player.radius/2)*targetInc, transform.position.z);
+			//print ("fwd " + player.transform.rotation.x); 
+			if (player.transform.rotation.x >= 0.15f){
+				print ("lean too fwd"); 
+				dm.fallen = true; 
+			}
+			else {
+				transform.position = new Vector3 (transform.position.x, transform.position.y - (dm.radius/2)*targetInc, transform.position.z);
+			}
 			break;
 		case (int) Dir.back:
-			transform.position = new Vector3 (transform.position.x, transform.position.y + (player.radius/2)*targetInc, transform.position.z);
+			//print ("bk " + player.transform.rotation.x); 
+			if (player.transform.rotation.x <= -0.15f){
+				print ("lean too bk"); 
+				dm.fallen = true; 
+			}
+			else {
+				transform.position = new Vector3 (transform.position.x, transform.position.y + (dm.radius/2)*targetInc, transform.position.z);
+			}
 			break;
 		case (int) Dir.right:
-			transform.position = new Vector3 (transform.position.x + (player.radius/2)*targetInc, transform.position.y, transform.position.z);
+			//print ("right, angle = " + player.transform.rotation.y); 
+
+			//.position = new Vector3 (transform.position.x + (dm.radius/2)*targetInc, transform.position.y, transform.position.z);
 			break;
 		case (int) Dir.left:
-			transform.position = new Vector3 (transform.position.x - (player.radius/2)*targetInc, transform.position.y, transform.position.z);
+			//transform.position = new Vector3 (transform.position.x - (dm.radius/2)*targetInc, transform.position.y, transform.position.z);
 			break; 
 		}
 	}
