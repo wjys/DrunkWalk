f45f3e17cd6c04c62db92a89999371a5dbf8541d
Anh Chi Bui <acqbui@gmail.com>
2014-07-18 15:09:08 -0400

    Cleaned Up DrunkMovement.cs

--- DrunkWalk/Assets/Scripts/BreakObject.cs
+++ DrunkWalk/Assets/Scripts/BreakObject.cs
@@ -10,7 +10,12 @@ public class BreakObject : MonoBehaviour {
 	public float shatterVelocity; 
 	public float shatterRadius;
 	public float shatterForce; 
-	
+
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * NO RETURN. ARG: Collider (with Trigger) of whatever the breakable object hits
+	 * (1) Check if the 
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+
 	void OnTriggerEnter (Collider col){
 		if (col.tag == "Floor"){
 			GameObject broken = Instantiate(brokenVase, transform.position, transform.rotation) as GameObject;
