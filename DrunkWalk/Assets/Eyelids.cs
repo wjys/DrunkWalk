b365282295c0d3d6641acf6d03d00ab172a3430d
wjys <w.jy.song@gmail.com>
2014-07-18 10:58:06 -0400

    Blinkcount >= 3 then pass out

--- DrunkWalk/Assets/Eyelids.cs
+++ DrunkWalk/Assets/Eyelids.cs
@@ -29,9 +29,9 @@ public class Eyelids : MonoBehaviour {
 	public float speed;
 
 	//Keep the original variables
-	private float sSpeed;
-	private float sAccel;
-	private float sWakeUp;
+	private static float sSpeed;
+	private static float sAccel;
+	private static float sWakeUp;
 	
 	private static bool gettingUp;
 	private static bool blinked;
@@ -69,8 +69,8 @@ public class Eyelids : MonoBehaviour {
 			//Eyelids falling
 			topLids.transform.position = new Vector3 (topLids.transform.position.x, topLids.transform.position.y - speed, topLids.transform.position.z);
 			bottomLids.transform.position = new Vector3 (bottomLids.transform.position.x, bottomLids.transform.position.y + speed, bottomLids.transform.position.z);
-
-
+			
+			//Curlout
 			if (BR.transform.rotation.z > 0){
 				BR.transform.rotation = new Quaternion (BR.transform.rotation.x, BR.transform.rotation.y, BR.transform.rotation.z - (speed * 0.01f), BR.transform.rotation.w);
 				BL.transform.rotation = new Quaternion (BL.transform.rotation.x, BL.transform.rotation.y, BL.transform.rotation.z + (speed * 0.01f), BR.transform.rotation.w);
@@ -80,7 +80,9 @@ public class Eyelids : MonoBehaviour {
 				TR.transform.rotation = new Quaternion (TR.transform.rotation.x, TR.transform.rotation.y, TR.transform.rotation.z + (speed * 0.01f), TR.transform.rotation.w);
 				TL.transform.rotation = new Quaternion (TL.transform.rotation.x, TL.transform.rotation.y, TL.transform.rotation.z - (speed * 0.01f), TR.transform.rotation.w);
 				}
+			
 
+			//Everytime you tap, eyelids flicker
 			if (me.buttonTapped){
 				topLids.transform.position = new Vector3 (topLids.transform.position.x, topLids.transform.position.y + wakeUp, topLids.transform.position.z);
 				bottomLids.transform.position = new Vector3 (bottomLids.transform.position.x, bottomLids.transform.position.y - wakeUp, bottomLids.transform.position.z);
@@ -91,7 +93,8 @@ public class Eyelids : MonoBehaviour {
 				TL.transform.rotation = new Quaternion (TL.transform.rotation.x, TL.transform.rotation.y, TL.transform.rotation.z + (wakeUp * 0.01f), TL.transform.rotation.w);
 				accel += 0.0001f;
 			}
-
+			
+			//If blinked 3 times, pass out.
 				if (topLids.transform.position.y <= (startPosUp.y-10) && blinkCnt < 3){
 					blinkCnt += 1;
 					blinked = true;
@@ -99,7 +102,7 @@ public class Eyelids : MonoBehaviour {
 					GameManager.ins.playerStatus = GameState.PlayerStatus.Lost;
 				}
 
-				if (topLids.transform.position.y >= (startPosUp.y-3)){
+				if (topLids.transform.position.y >= (startPosUp.y-(2*blinkCnt))){
 					blinked = false;
 				}
 
@@ -112,6 +115,9 @@ public class Eyelids : MonoBehaviour {
 
 	void FixedUpdate() {
 		if (gettingUp){
+			speed = sSpeed;
+			accel = sAccel;
+			wakeUp = sWakeUp;
 
 			lidCurl();
 
@@ -119,9 +125,10 @@ public class Eyelids : MonoBehaviour {
 		}
 
 		if (blinked){
-			speed = sSpeed + 0.0005f;
-			accel = sAccel + 0.0005f;
-			wakeUp = sWakeUp - 0.2f;
+
+			speed = sSpeed + (0.0005f * blinkCnt);
+			accel = sAccel + (0.0005f * blinkCnt);
+			wakeUp = sWakeUp - (0.2f * blinkCnt);
 
 			lidCurl ();
 		}
@@ -129,6 +136,7 @@ public class Eyelids : MonoBehaviour {
 	}
 
 	public void lidCurl(){
+
 		topLids.transform.position = Vector3.Lerp(topLids.transform.position, startPosUp, smooth * Time.deltaTime);
 		bottomLids.transform.position = Vector3.Lerp(bottomLids.transform.position, startPosDown, smooth * Time.deltaTime);
 
