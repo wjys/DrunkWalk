d7a855eede307e6b5b613d254a0a741c58666dbb
Anh Chi Bui <acqbui@gmail.com>
2014-07-31 16:32:44 -0400

    Divided UniMoveManager into UniMoveSplash and UniMoveGame + More Changes/Additions to Scripts

--- DrunkWalk/Assets/Scripts/UniMove/UniMoveGame.cs
+++ DrunkWalk/Assets/Scripts/UniMove/UniMoveGame.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections.Generic;
+using System.Collections;
 
-public class UniMoveManager : MonoBehaviour 
-{
-	// We save a list of Move controllers.
-	public List<UniMoveController> moves = new List<UniMoveController>();
+// TURN THIS ON WHEN IN GAME
+
+public class UniMoveGame : MonoBehaviour {
+
 	public GameObject[] players;
 	public GameObject player; 
-	public int moveCount; 
+	public UniMoveController[] moves;
+	public int playerCount; 
 	public int numPlayers;
 	private bool createPlayer;
 	private bool setPlayer;
@@ -20,15 +20,14 @@ public class UniMoveManager : MonoBehaviour
 	
 	void Start() 
 	{
-		if (numPlayers < 4) {
-			int num = numPlayers+1;
-			if (GameManager.ins.status != GameState.GameStatus.Splash){
-				while (num <= 4){
-					GameObject.Find ("UICam " + num).SetActive(false);
-					num++;
-				}
+		playerCount = 0;
+		players = new GameObject[numPlayers - 1];
+		moves = gameObject.GetComponents<UniMoveController> ();
+		/*if (numPlayers <= 4) {
+			for (int num = 1; num <= 4; num++){
+				GameObject.Find ("UICam " + num).SetActive(false);
 			}
-		}
+		}*/
 		players = new GameObject[numPlayers];
 		
 		// CAMERA VIEWPORT
@@ -46,128 +45,29 @@ public class UniMoveManager : MonoBehaviour
 		
 		
 		positions = new Vector3[4] { 	new Vector3 (-1.10f, 1.424898f, 3.941933f),
-			new Vector3 (-0.03585815f, 1.424898f, 3.941933f),
-			new Vector3 (1.16f, 1.424898f, 3.941933f), 
-			new Vector3 (2.383401f, 1.424898f, 3.366474f)};
+										new Vector3 (-0.03585815f, 1.424898f, 3.941933f),
+										new Vector3 (1.16f, 1.424898f, 3.941933f), 
+										new Vector3 (2.383401f, 1.424898f, 3.366474f)};
 		
 		rotations = new Quaternion (0, 0, 0, 0);
 		
-		moveCount = 0; 
-		UniMoveInit (); 
 		setPlayer = false; 
 		
-		if (GameManager.ins.status == GameState.GameStatus.Tutorial){
-			positions = new Vector3[1] {	new Vector3 (0, 1.424898f, -6)};
-		}
+//		if (GameManager.ins.status == GameState.GameStatus.Tutorial){
+//			positions = new Vector3[1] { new Vector3 (0, 1.424898f, -6) };
+//		}
 	}
 	
 	
 	void Update() 
 	{
 		if (StopManager () == false) {
-			UniMoveSetID ();
-			if (createPlayer){
-				createPlayers();
-				print ("createPlayers() done, UniMoveSetPlayers() starting");
-				UniMoveSetPlayers ();
-			}
+			createPlayers();
 		} 
 		else {
-			if (GameManager.ins.status != GameState.GameStatus.Splash){
-				setUI ();
-				UniMoveActivateComponents();
-				this.enabled = false;
-			}
-		}
-	}
-	
-	void HandleControllerDisconnected (object sender, EventArgs e)
-	{
-		// TODO: Remove this disconnected controller from the list and maybe give an update to the player
-	}
-
-
-
-	/* --------------------------------------------------------------------------------------------------------------------------
-	 * NO ARG. NO RETURN.
-	 * API Code to initialize the move controllers within this manager 
-	 * -------------------------------------------------------------------------------------------------------------------------- */
-
-	private void UniMoveInit(){
-		Time.maximumDeltaTime = 0.05f;
-		
-		int count = UniMoveController.GetNumConnected();
-		
-		// Iterate through all connections (USB and Bluetooth)
-		for (int i = 0; i < count; i++) 
-		{	
-			UniMoveController move = gameObject.AddComponent<UniMoveController>();	// It's a MonoBehaviour, so we can't just call a constructor
-
-			// Remember to initialize!
-			if (!move.Init(i)) 
-			{	
-				Destroy(move);	// If it failed to initialize, destroy and continue on
-				continue;
-			}
-			
-			// This example program only uses Bluetooth-connected controllers
-			PSMoveConnectionType conn = move.ConnectionType;
-			if (conn == PSMoveConnectionType.Unknown || conn == PSMoveConnectionType.USB) 
-			{
-				Destroy(move);
-			}
-			else 
-			{
-				moves.Add(move);
-				
-				move.OnControllerDisconnected += HandleControllerDisconnected;
-
-			}
-		}
-	}
-
-	/* --------------------------------------------------------------------------------------------------------------------------
-	 * NO ARG. NO RETURN.
-	 * when a player taps the Move button on their controller, set the ID on the move controller to the first available player
-	 * -------------------------------------------------------------------------------------------------------------------------- */
-
-	private void UniMoveSetID(){
-		foreach (UniMoveController move in moves){
-			//print (move.id);
-			if (move.id == 0){
-				if (move.GetButtonUp(PSMoveButton.Move)){
-					switch (moveCount){
-					case 0:
-						move.SetLED (Color.cyan);
-						move.id = 1;
-						createPlayer = true;
-						return;
-					case 1:
-						if (move.id == 0){
-							move.SetLED (Color.red);
-							move.id = 2;
-							createPlayer = true;
-						}
-						return;
-					case 2:
-						if (move.id == 0){
-							move.SetLED (Color.yellow);
-							move.id = 3;
-							createPlayer = true;
-						}
-						return;
-					case 3:
-						if (move.id == 0){
-							move.SetLED (Color.magenta);
-							move.id = 4; 
-							createPlayer = true;
-						}
-						return;
-					default:
-						return;
-					}
-				}
-			}
+			setUI ();
+			UniMoveActivateComponents();
+			this.enabled = false;
 		}
 	}
 
@@ -177,97 +77,70 @@ public class UniMoveManager : MonoBehaviour
 	 * (1) set up the viewport rect of the player cameras
 	 * (2) set the appropriate ID to the player 
 	 * -------------------------------------------------------------------------------------------------------------------------- */
-
+	
 	private void createPlayers(){
-		players [moveCount] = Instantiate (player, positions [moveCount], rotations) as GameObject;
-
+		players [playerCount] = Instantiate (player, positions [playerCount], rotations) as GameObject;
+		
 		// SET UP THE ID OF THE HEAD AND COMPONENTS
-		players [moveCount].name = "Head " + (moveCount + 1);
-		players [moveCount].GetComponent<DrunkMovement> ().id = moveCount + 1;
-		players [moveCount].GetComponent<UniMoveDisplay> ().id = moveCount + 1; 
-
+		players [playerCount].name = "Head " + (playerCount + 1);
+		players [playerCount].GetComponent<DrunkMovement> ().id = playerCount + 1;
+		players [playerCount].GetComponent<UniMoveDisplay> ().id = playerCount + 1; 
+		
 		// SET UP THE CAMERAS (ON HEAD'S CHILDREN)
 		Camera[] cams;
-
-		switch (moveCount) {
 		
+		switch (playerCount) {
+			
 		// 1 player
 		case 0:
-			cams = players [moveCount].GetComponentsInChildren<Camera> ();
+			cams = players [playerCount].GetComponentsInChildren<Camera> ();
 			foreach (Camera cam in cams){
 				cam.rect = (rects[6]);
 			}
-			break;
-
-		// 2 players
+			playerCount++;
+			return;
+			
+			// 2 players
 		case 1:
-			for (int i = 0; i <= moveCount; i++){
+			for (int i = 0; i <= playerCount; i++){
 				cams = players[i].GetComponentsInChildren<Camera>();
 				foreach (Camera cam in cams){
 					cam.rect = (rects [i+4]);
 					//print (i + " " + cam.rect);
 				}
 			}
-			break;
-
-		// 3 players
+			playerCount++;
+			return;
+			
+			// 3 players
 		case 2:
-			for (int i = 0; i <= moveCount; i++){
+			for (int i = 0; i <= playerCount; i++){
 				cams = players[i].GetComponentsInChildren<Camera>();
 				foreach (Camera cam in cams){
 					cam.rect = (rects [i]);
 				}
 			}
-			break;
-
-		// 4 players
+			playerCount++;
+			return;
+			
+			// 4 players
 		case 3:
-			cams = players [moveCount].GetComponentsInChildren<Camera> ();
+			cams = players [playerCount].GetComponentsInChildren<Camera> ();
 			foreach (Camera cam in cams){
 				cam.rect = (rects[3]);
 			}
-			break;
+			playerCount++;
+			return;
 		default:
-			break;
+			return;
 		}
-	}
-
-	/* --------------------------------------------------------------------------------------------------------------------------
-	 * NO ARG. NO RETURN.
-	 * set the UniMove controller to the player (add component to the player)
-	 * -------------------------------------------------------------------------------------------------------------------------- */
-
-	private void UniMoveSetPlayers(){ 
-		//print ("entered UniMoveSetPlayers()");
-		for(int i = 0; i < moves.Count; i++){
-			UniMoveController mv; 
-			//print ("checking move id" + moves[i].id);
-
-			if (moves[i].id > 0){
-				mv = players[moveCount].GetComponent<UniMoveController>();
-				if (mv == null){
-					UniMoveDisplay display = players[moveCount].GetComponent<UniMoveDisplay>();
-					if (moves[i].id == display.id){
-						//print ("adding move " + moves[i].id + " to player " + display.id);
-						mv = players[moveCount].AddComponent<UniMoveController>() as UniMoveController;
-						mv.Init (i); 
-						mv.id = display.id;
-						createPlayer = false;
-						moveCount++;
-						break;
-					}
-					//print ("finished adding move to player");
-				}
-			}
-		}
-		//print ("exiting UniMoveSetPlayers()");
 	}
 
 	/* --------------------------------------------------------------------------------------------------------------------------
 	 * NO ARG. NO RETURN.
 	 * set UICam viewports and assign the scripts the children's components call
 	 * -------------------------------------------------------------------------------------------------------------------------- */
-
+	
 	private void setUI(){
 		Camera ui;
 		
@@ -275,20 +148,20 @@ public class UniMoveManager : MonoBehaviour
 		if (numPlayers == 1) {
 			ui = GameObject.Find ("UICam 1").camera;
 			ui.rect = rects[6];
-
+			
 			Eyelids eyelids = ui.GetComponentInChildren <Eyelids>();
 			Compass comp = ui.GetComponentInChildren<Compass>();
 			Ouch ouch = ui.GetComponentInChildren<Ouch>();
-
+			
 			eyelids.me = GameObject.Find ("Head 1").GetComponent<DrunkMovement>();
 			comp.me =  GameObject.Find ("Head 1");
 			ouch.collision = GameObject.Find ("Head 1").GetComponent<Collision>();
 			ouch.dm = GameObject.Find ("Head 1").GetComponent<DrunkMovement>();
-
+			
 			eyelids.enabled = true;
 			comp.enabled = true;
 			ouch.enabled = true;
-
+			
 			foreach (SpriteRenderer sprite in ui.GetComponentsInChildren<SpriteRenderer>()){
 				sprite.enabled = true;
 			}
@@ -299,11 +172,11 @@ public class UniMoveManager : MonoBehaviour
 			for (int i = 1; i < numPlayers+1; i++){
 				ui = GameObject.Find ("UICam " + i).camera;
 				ui.rect = rects[i+3];
-
+				
 				Eyelids eyelids = ui.GetComponentInChildren <Eyelids>();
 				Compass comp = ui.GetComponentInChildren<Compass>();
 				Ouch ouch = ui.GetComponentInChildren<Ouch>();
-
+				
 				eyelids.me = GameObject.Find ("Head " + i).GetComponent<DrunkMovement>();
 				comp.me =  GameObject.Find ("Head " + i);
 				ouch.collision = GameObject.Find ("Head " + i).GetComponent<Collision>();
@@ -312,7 +185,7 @@ public class UniMoveManager : MonoBehaviour
 				eyelids.enabled = true;
 				comp.enabled = true;
 				ouch.enabled = true;
-
+				
 				foreach (SpriteRenderer sprite in ui.GetComponentsInChildren<SpriteRenderer>()){
 					sprite.enabled = true;
 				}
@@ -328,7 +201,7 @@ public class UniMoveManager : MonoBehaviour
 				Eyelids eyelids = ui.GetComponentInChildren <Eyelids>();
 				Compass comp = ui.GetComponentInChildren<Compass>();
 				Ouch ouch = ui.GetComponentInChildren<Ouch>();
-
+				
 				eyelids.me = GameObject.Find ("Head " + i).GetComponent<DrunkMovement>();
 				comp.me =  GameObject.Find ("Head " + i);
 				ouch.collision = GameObject.Find ("Head " + i).GetComponent<Collision>();
@@ -337,32 +210,32 @@ public class UniMoveManager : MonoBehaviour
 				eyelids.enabled = true;
 				comp.enabled = true;
 				ouch.enabled = true;
-
+				
 				foreach (SpriteRenderer sprite in ui.GetComponentsInChildren<SpriteRenderer>()){
 					sprite.enabled = true;
 				}
-
+				
 				ui.gameObject.SetActive(true);
 			}
 		}
 	}
-
-
+	
+	
 	/* --------------------------------------------------------------------------------------------------------------------------
 	 * NO ARG. NO RETURN.
 	 * when all the players have been set, activate the player components
 	 * -------------------------------------------------------------------------------------------------------------------------- */
-
+	
 	private void UniMoveActivateComponents(){
 		for (int i = 0; i < numPlayers; i++) {
 			UniMoveController mv = players [i].GetComponent<UniMoveController> ();
-
+			
 			if (mv != null) {
 				DrunkMovement dm = players [i].GetComponent<DrunkMovement> (); 
 				Rotation rot = players [i].GetComponent<Rotation> ();
 				DrunkForce df = players [i].GetComponent<DrunkForce> ();
 				Collision col = players [i].GetComponent<Collision> ();
-
+				
 				dm.enabled = true;
 				rot.enabled = true;
 				df.enabled = true;
@@ -370,9 +243,9 @@ public class UniMoveManager : MonoBehaviour
 			}
 		}
 	}
-
+	
 	private bool StopManager(){
-		if (moveCount == numPlayers){
+		if (playerCount >= numPlayers){
 			return true;
 		}
 		return false; 
---
+++ DrunkWalk/Assets/Scripts/UniMove/UniMoveSplash.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class UniMoveSplash : MonoBehaviour 
+{
+	// We save a list of Move controllers.
+	public List<UniMoveController> moves = new List<UniMoveController>();
+	public int numPlayers;
+
+	void Start() 
+	{
+		numPlayers = 0; 
+		UniMoveInit (); 
+	}
+	
+	
+	void Update() 
+	{
+		setMoveColour ();
+		UniMoveSetID ();
+		if (numPlayers > 1) {
+			// DISPLAY MULTIPLAYER MENU OPTION
+			print ("DISPLAY MULTIPLAYER OPTION");
+		}
+		/* if (levelSelect){
+		 * 		if (UniMoveAllPlayersIn()){
+		 * 			// change the game state i.e. leaving splash screen
+		 * 			// Application.LoadLevel(SOMELEVEL);
+		 * 			setGame();
+		 * 			this.enabled = false;
+		 * 		}
+		 * }
+		 */
+		if (UniMoveAllPlayersIn ()) {
+			setGame ();
+			this.enabled = false;
+		}
+	}
+	
+	void HandleControllerDisconnected (object sender, EventArgs e)
+	{
+		// TODO: Remove this disconnected controller from the list and maybe give an update to the player
+	}
+	
+	
+	
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * NO ARG. NO RETURN.
+	 * API Code to initialize the move controllers within this manager 
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+	
+	private void UniMoveInit(){
+		Time.maximumDeltaTime = 0.05f;
+		
+		int count = UniMoveController.GetNumConnected();
+		
+		// Iterate through all connections (USB and Bluetooth)
+		for (int i = 0; i < count; i++) 
+		{	
+			UniMoveController move = gameObject.AddComponent<UniMoveController>();	// It's a MonoBehaviour, so we can't just call a constructor
+			
+			// Remember to initialize!
+			if (!move.Init(i)) 
+			{	
+				Destroy(move);	// If it failed to initialize, destroy and continue on
+				continue;
+			}
+			
+			// This example program only uses Bluetooth-connected controllers
+			PSMoveConnectionType conn = move.ConnectionType;
+			if (conn == PSMoveConnectionType.Unknown || conn == PSMoveConnectionType.USB) 
+			{
+				Destroy(move);
+			}
+			else 
+			{
+				moves.Add(move);
+				
+				move.OnControllerDisconnected += HandleControllerDisconnected;
+				
+			}
+		}
+	}
+	
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * NO ARG. NO RETURN.
+	 * when a player taps the Move button on their controller, set the ID on the move controller to the first available player
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+	
+	private void UniMoveSetID(){
+		foreach (UniMoveController move in moves){
+			//print (move.id);
+			if (move.id == 0){
+				if (move.GetButtonUp(PSMoveButton.Move)){
+					switch (numPlayers){
+					case 0:
+						move.SetLED (Color.cyan);
+						move.id = 1;
+						numPlayers = 1;
+						return;
+					case 1:
+						move.SetLED (Color.red);
+						move.id = 2;
+						numPlayers = 2;
+						return;
+					case 2:
+						move.SetLED (Color.yellow);
+						move.id = 3;
+						numPlayers = 3;
+						return;
+					case 3:
+						move.SetLED (Color.magenta);
+						move.id = 4; 
+						numPlayers = 4;
+						return;
+					default:
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * NO ARG. RETURN BOOL: true if ready to move on to level select
+	 * if there are 4 players in or if all 2+ players are holding move button, move on to next game state/level select
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+
+	private bool UniMoveAllPlayersIn(){
+		bool holdMove = true;
+		if (numPlayers > 1){
+			if (numPlayers == 4) {
+				return true;
+			}
+
+			foreach (UniMoveController move in moves) {
+				if (move.id != 0){
+					if (!move.GetButton (PSMoveButton.Move)){
+						holdMove = false;
+					}
+				}
+			}
+			if (holdMove) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * SET MOVE CONTROLLER COLOUR
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+	
+	private void setMoveColour(){
+		foreach (UniMoveController move in moves){
+			switch (move.id) {
+			case 0:
+				break;
+			case 1:
+				move.SetLED (Color.cyan);
+				break;
+			case 2:
+				move.SetLED (Color.red);
+				break;
+			case 3:
+				move.SetLED (Color.yellow);
+				break;
+			case 4:
+				move.SetLED (Color.magenta);
+				break;
+			default:
+				break;
+
+			}
+		}
+	}
+
+	/* --------------------------------------------------------------------------------------------------------------------------
+	 * SET NECESSARY VARIABLES FOR THE GAME IN THE UNIMOVEGAME MANAGER SCRIPT
+	 * -------------------------------------------------------------------------------------------------------------------------- */
+	
+	private void setGame(){
+		UniMoveGame gm = gameObject.GetComponent<UniMoveGame> ();
+		gm.numPlayers = numPlayers;
+
+		gm.enabled = true;
+	}
+}
