bf6241edd75e0f23b05ef60125f37f77007015cd
Anh Chi Bui <acqbui@gmail.com>
2014-08-13 10:35:22 -0400

    End/Results Screen for Multiplayer Modes BOTH OK!

--- DrunkWalk/Assets/Scripts/Manager/EndScreen.cs
+++ DrunkWalk/Assets/Scripts/Manager/EndScreen.cs
@@ -9,10 +9,16 @@ public class EndScreen : MonoBehaviour {
 	public bool getMoves;
 	public UniMoveController[] moves;
 	public UniMoveController[] pairedMoves;
+	public Sprite[] winSprites;
+	public Sprite[] loseSprites;
+	public Sprite[] numSprites;
+
+	public bool spritesSet;
 	
 	// Use this for initialization
 	void Start () {
 		soundPlayed = false; 
+		spritesSet = false;
 	}
 	
 	// Update is called once per frame
@@ -32,10 +38,24 @@ public class EndScreen : MonoBehaviour {
 			}
 			soundPlayed = true; 
 		}*/
+		if (GameManager.ins.mode == GameState.GameMode.ScoreAttack){
+			if (Input.anyKey){
+				Application.LoadLevel ("Scores");
+				this.enabled = false;
+			}
+		}
+		else if (GameManager.ins.mode == GameState.GameMode.Party || GameManager.ins.mode == GameState.GameMode.Race){
 
-		if (Input.anyKey){
-			Application.LoadLevel ("Scores");
-			this.enabled = false;
+			//if (!spritesSet){
+				setSprites();
+			//}
+
+			if (Input.anyKey){
+				Destroy(GameObject.Find("GameState"));
+				Application.LoadLevel ("Splash");
+				Destroy (this.gameObject);
+				this.enabled = false;
+			}
 		}
 	}
 
@@ -43,4 +63,219 @@ public class EndScreen : MonoBehaviour {
 		moves = gameObject.GetComponents<UniMoveController>();
 		getMoves = true;
 	}
+
+	private void setSprites(){
+		GameObject race, party;
+		race = GameObject.Find ("RaceEnd");
+		party = GameObject.Find ("PartyEnd");
+		
+		if (GameManager.ins.mode == GameState.GameMode.Race){
+			//IF RACE RESULTS
+			if (party != null){
+				Destroy (party);
+			}
+
+			getRaceWinSprites();
+			getRaceLoseSprites();
+
+			//if 1 player is in the winner index, enable spriterenderer of Win1 with its Player & Character ID
+			//	show Lose1, Lose2, Lose3 in the loser index with their IDs
+			
+			//if 2 players are in the winner index, show Win1, Win2 with their IDs
+			//	show Lose1, Lose2 in the loser index with their IDs
+			
+			//if 3 players are in the winner index, show Win1, Win2, Win3 with their IDs
+			// show Lose1 with its ID
+			
+		} else if (GameManager.ins.mode == GameState.GameMode.Party){
+			//IF PARTY RESULTS
+			if (race != null){
+				Destroy (race);
+			}
+
+			getPartyWinSprites ();
+			getPartyLoseSprites();
+
+			//Send appropriate Player ID and character ID to 1st, 2nd, 3rd, 4th place sprite renderer
+		}
+		spritesSet = true;
+	}
+	private void getPartyWinSprites(){
+		for (int i = 0; i < GameManager.ins.winnerIndex; i++){
+			SpriteRenderer[] sprites = GameObject.Find ("Win" + (i+1)).GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer spr in sprites){
+				if (spr.gameObject.name.Equals ("PlayerNum")){
+					switch (GameManager.ins.winners [i]){
+					case 1:
+						spr.sprite = numSprites[0];
+						break;
+					case 2:
+						spr.sprite = numSprites[1];
+						break;
+					case 3:
+						spr.sprite = numSprites[2];
+						break;
+					case 4:
+						spr.sprite = numSprites[3];
+						break;
+					}
+					spr.enabled = true;
+					spr.gameObject.GetComponent<PlayerNum>().enabled = true;
+				}
+				else {
+					switch (GameManager.ins.multiChosenChar [GameManager.ins.winners [i]-1]){
+					case 1:
+						spr.sprite = winSprites[0];
+						break;
+					case 2:
+						spr.sprite = winSprites[1];
+						break;
+					case 3:
+						spr.sprite = winSprites[2];
+						break;
+					case 4:
+						spr.sprite = winSprites[3];
+						break;
+					}
+					spr.enabled = true;
+				}
+			}
+		}
+	}
+
+	private void getPartyLoseSprites(){
+		for (int i = 0; i < GameManager.ins.loserIndex; i++){
+			SpriteRenderer[] sprites = GameObject.Find ("Lose" + (i+1)).GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer spr in sprites){
+				if (spr.gameObject.name.Equals ("PlayerNum")){
+					switch (GameManager.ins.losers [i]){
+					case 1:
+						spr.sprite = numSprites[0];
+						break;
+					case 2:
+						spr.sprite = numSprites[1];
+						break;
+					case 3:
+						spr.sprite = numSprites[2];
+						break;
+					case 4:
+						spr.sprite = numSprites[3];
+						break;
+					}
+					spr.enabled = true;
+					spr.gameObject.GetComponent<PlayerNum>().enabled = true;
+				}
+				else {
+					switch (GameManager.ins.multiChosenChar [GameManager.ins.losers [i]-1]){
+					case 1:
+						spr.sprite = loseSprites[0];
+						break;
+					case 2:
+						spr.sprite = loseSprites[1];
+						break;
+					case 3:
+						spr.sprite = loseSprites[2];
+						break;
+					case 4:
+						spr.sprite = loseSprites[3];
+						break;
+					}
+					spr.enabled = true;
+				}
+			}
+		}
+	}
+
+	private void getRaceWinSprites(){
+		for (int i = 0; i < GameManager.ins.winnerIndex; i++){
+			SpriteRenderer[] sprites = GameObject.Find ("RWin" + (i+1)).GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer spr in sprites){
+				if (spr.gameObject.name.Equals ("PlayerNum")){
+					switch (GameManager.ins.winners [i]){
+					case 1:
+						spr.sprite = numSprites[0];
+						break;
+					case 2:
+						spr.sprite = numSprites[1];
+						break;
+					case 3:
+						spr.sprite = numSprites[2];
+						break;
+					case 4:
+						spr.sprite = numSprites[3];
+						break;
+					}
+					spr.enabled = true;
+					spr.gameObject.GetComponent<PlayerNum>().enabled = true;
+				}
+				else if (spr.gameObject.name.Equals ("EndRoute")){
+					spr.enabled = true;
+				}
+				else {
+					switch (GameManager.ins.multiChosenChar [GameManager.ins.winners [i]-1]){
+					case 1:
+						spr.sprite = winSprites[0];
+						break;
+					case 2:
+						spr.sprite = winSprites[1];
+						break;
+					case 3:
+						spr.sprite = winSprites[2];
+						break;
+					case 4:
+						spr.sprite = winSprites[3];
+						break;
+					}
+					spr.enabled = true;
+				}
+			}
+		}
+	}
+	
+	private void getRaceLoseSprites(){
+		for (int i = 0; i < GameManager.ins.loserIndex; i++){
+			SpriteRenderer[] sprites = GameObject.Find ("RLose" + (i+1)).GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer spr in sprites){
+				if (spr.gameObject.name.Equals ("PlayerNum")){
+					switch (GameManager.ins.losers [i]){
+					case 1:
+						spr.sprite = numSprites[0];
+						break;
+					case 2:
+						spr.sprite = numSprites[1];
+						break;
+					case 3:
+						spr.sprite = numSprites[2];
+						break;
+					case 4:
+						spr.sprite = numSprites[3];
+						break;
+					}
+					spr.enabled = true;
+					spr.gameObject.GetComponent<PlayerNum>().enabled = true;
+				}
+				else if (spr.gameObject.name.Equals ("EndRoute")){
+					spr.enabled = true;
+				}
+				else {
+					switch (GameManager.ins.multiChosenChar [GameManager.ins.losers [i]-1]){
+					case 1:
+						spr.sprite = loseSprites[0];
+						break;
+					case 2:
+						spr.sprite = loseSprites[1];
+						break;
+					case 3:
+						spr.sprite = loseSprites[2];
+						break;
+					case 4:
+						spr.sprite = loseSprites[3];
+						break;
+					}
+					spr.enabled = true;
+				}
+			}
+		}
+	}
+
 }
